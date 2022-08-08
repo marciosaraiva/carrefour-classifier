9b27b7e5caa1f67146970d3f4241d449ec7078b6
Márcio Saraiva <marciosaraivac2@gmail.com>
2022-08-09 00:08:31 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,30 +1,19 @@
 from flask import Flask, request
 import project_functions as pf
-import gc
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Marcio! Try to access my site: <a href=http://marciosaraiva.pythonanywhere.com/classify/<url> link </a>'
+    return 'Hello from Marcio! Try to access my site: <a href=http://marciosaraiva.pythonanywhere.com/classify?url=link </a>'
 
 @app.route('/classify')
 def run_classification():
     try:
         url = request.args.get("url")
-        gc.collect()
         classification = pf.get_classification(url)
-        gc.collect()
         return {"Classification": classification}
     except Exception as e:
         detail = str(e)
         return {"Error": "Impossible to classify",
                 "Error detail": detail}
-    gc.collect()
-
-
-@app.route('/teste/<texto>')
-def run_teste(texto):
-    return "um texto "+ texto
-
-
-
